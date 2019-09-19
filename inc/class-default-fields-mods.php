e55b63d01a9380424eb59321b200220e92eb9d19
Mike Estrada <mestrada@nu.edu>
2019-09-19 10:08:44 -0700

    Add markup change to wrap the label around the option instead of placed below

--- inc/class-default-fields-mods.php
+++ inc/class-default-fields-mods.php
@@ -21,6 +21,7 @@ class Default_Fields_Mods {
 		add_action( 'gform_field_appearance_settings', [ $this, 'radio_choices_label_setting' ], 10, 2 );
 		add_action( 'gform_editor_js', [ $this, 'editor_script' ] );
 		add_filter( 'gform_tooltips', [ $this, 'add_encryption_tooltips' ] );
+		add_filter( 'gform_field_choice_markup_pre_render', [ $this, 'set_up_radio_label' ], 10, 4 );
 	}
 
 	/**
@@ -90,5 +91,40 @@ class Default_Fields_Mods {
 	public function add_encryption_tooltips( $tooltips ) {
 		$tooltips['radio_choices_label_tip_value'] = '<h6>Label Position/Behavior</h6>Select if the label should be echoed out below the actual radio option markup or it should be wrapped around the option.';
 		return $tooltips;
+	}
+
+	/**
+	 * Undocumented function
+	 *
+	 * @param string $choice_markup The markup for the field including the wrapping <li>, label, and input (if it has not been altered from the native markup).
+	 * @param array  $choice        Array of options pertaining to the specific choice, including text, value, and isSelected.
+	 * @param object $field         The full Gravity Form field pertaining to the choice/radio option.
+	 * @param string $value         Value of the radio option.
+	 *
+	 * @return string
+	 */
+	public function set_up_radio_label( $choice_markup, $choice, $field, $value ) {
+		if ( is_admin() || 'radio' !== $field->type || empty( $field->labelBehavior ) ) { // phpcs:ignore WordPress.NamingConventions.ValidVariableName
+			return $choice_markup;
+		}
+
+		// Pull the input field and label.
+		preg_match( '/<input(.*?)>/i', $choice_markup, $input_matches );
+		preg_match( '/<label(.*?)>/i', $choice_markup, $label_matches );
+
+		// If the matches do not exist, we can't really do what we want so bail.
+		if ( empty( $input_matches[0] ) || empty( $label_matches[0] ) ) {
+			return $choice_markup;
+		}
+
+		$input_field    = $input_matches[0];
+		$label_open_tag = $label_matches[0];
+
+		// Remove the input field from the original markup
+		// then append it after the starting tag for the label.
+		$choice_markup = str_replace( $input_field, '', $choice_markup );
+		$choice_markup = str_replace( $label_open_tag, $label_open_tag . $input_field, $choice_markup );
+
+		return $choice_markup;
 	}
 }
