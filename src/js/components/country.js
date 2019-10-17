1efc4ea4c071183158710ea50123139987118704
Mike Estrada <mestrada@nu.edu>
2019-10-16 17:45:11 -0700

    Test an error

--- src/js/components/country.js
+++ src/js/components/country.js
@@ -25,7 +25,7 @@
 			if ( zipcodeWrapper.length && ( 'US' !== isoCode || 'USA' !== countryName ) ) {
 				zipcodeWrapper.find( 'input' ).val( '00000' );
 				zipcodeWrapper.addClass('form__group--active');
-			} else if ( zipcodeWrapper.length ) {
+			} elseif ( zipcodeWrapper.length ) {
 				zipcodeWrapper.find( 'input' ).val( '' );
 				zipcodeWrapper.removeClass( 'form__group--active' );
 			}
